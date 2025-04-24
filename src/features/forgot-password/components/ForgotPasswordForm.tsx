25d88493f37278327bc7dcda13767d3d35945f03
diarifiana <diarifiana@gmail.com>
2025-04-24 14:34:34 +0700

    Reset password (#2)
    * feat: forgot-password
    
    * reset password
    
    * reset password

--- src/features/forgot-password/components/ForgotPasswordForm.tsx
+++ src/features/forgot-password/components/ForgotPasswordForm.tsx
@@ -10,23 +10,23 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import useLogin from "@/hooks/api/auth/useLogin";
+import useForgotPassword from "@/hooks/api/auth/useForgotPassword";
 import { cn } from "@/lib/utils";
 import { useFormik } from "formik";
 import Link from "next/link";
-import { LoginSchema } from "../schemas";
+import { ForgotPasswordSchema } from "../schemas";
 
-export function LoginForm({
+export function ForgotPasswordForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
-  const { mutateAsync: login, isPending } = useLogin();
+  const { mutateAsync: forgotPassword, isPending } = useForgotPassword();
 
   const formik = useFormik({
-    initialValues: { email: "", password: "" },
-    validationSchema: LoginSchema,
+    initialValues: { email: "" },
+    validationSchema: ForgotPasswordSchema,
     onSubmit: async (values) => {
-      await login(values);
+      await forgotPassword(values);
     },
   });
 
@@ -34,8 +34,8 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>Enter your credentials to sign in</CardDescription>
+          <CardTitle className="text-2xl">Forgot Password</CardTitle>
+          <CardDescription>Input your email to reset password</CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={formik.handleSubmit}>
@@ -56,29 +56,9 @@ export function LoginForm({
                   <p className="text-xs text-red-500">{formik.errors.email}</p>
                 )}
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
 
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Your Password"
-                  required
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.touched.password && !!formik.errors.password && (
-                  <p className="text-xs text-red-500">
-                    {formik.errors.password}
-                  </p>
-                )}
-              </div>
               <Button type="submit" className="w-full" disabled={isPending}>
-                {isPending ? "Loading" : "Login"}
+                {isPending ? "Loading" : "Reset Password"}
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
