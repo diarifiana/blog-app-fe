91a7b98eccc816c59d177b06eb17c608a44cad74
diarifiana <diarifiana@gmail.com>
2025-04-25 14:05:32 +0700

    feat: add blog list

---
+++ src/features/home/components/Jumbotron.tsx
@@ -0,0 +1,10 @@
+const Jumbotron = () => {
+  return (
+    <section className="mt-10 space-y-1 text-center">
+      <h1 className="text-6xl font-bold">The Food Ninja Blog</h1>
+      <p className="text-xl">Blog about food and recipe</p>
+    </section>
+  );
+};
+
+export default Jumbotron;
